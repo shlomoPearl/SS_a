94165cfed34f4fe03f6c5095d1cf05c4024bfdf9
shlomoPearl <93203179+shlomoPearl@users.noreply.github.com>
2021-11-08 09:19:03 +0200

    v3

--- main.c
+++ main.c
@@ -6,35 +6,35 @@ int main(){
 	int b;
 	//printf("enter 2 numbers:\n");
 	scanf("%d%d",&a,&b);
-	printf("The Armstrong numbers are: ");
+	printf("The Armstrong numbers are:");
 	for(int i=a;i<=b;i++){
 		int ans1 = isArmstrong(i);
 		if(ans1 !=0){
-			printf("%d ",i);
+			printf(" %d",i);
 		}
 	}
 	printf("\n");
-	printf("The Palindromes are: ");
+	printf("The Palindromes are:");
 	for(int i=a;i<=b;i++){
 		int ans2 = isPalindrome(i);
 		if(ans2 !=0){
-			printf("%d ",i);
+			printf(" %d",i);
 		}
 	}
 	printf("\n");
-	printf("The Prime numbers are: ");
+	printf("The Prime numbers are:");
 	for(int i=a;i<=b;i++){
 		int ans3 = isPrime(i);
 		if(ans3 !=0){
-			printf("%d ",i);
+			printf(" %d ",i);
 		}
 	}
 	printf("\n");
-	printf("The Strong numbers are: ");
+	printf("The Strong numbers are:");
 	for(int i=a;i<=b;i++){
 		int ans4 = isStrong(i);
 		if(ans4 !=0){
-			printf("%d ",i);
+			printf(" %d",i);
 		}
 	}
 	printf("\n");
